d9e8a3b6523a799d0f01f66a96b70bbb36ee1b00
mkrasnansk <miso.krasnansky@gmail.com>
2023-04-25 22:30:06 +0200

    backdrop init in redux

---
+++ src/components/backdrop.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Backdrop, CircularProgress } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { visibleBackdrop } from "../store/features/preloaders/backdropSlice";
+
+export const BackdropPreloader = () => {
+	const dispatch = useDispatch();
+	const viewBackdrop = useSelector((state) => state.backdropView.value);
+	return (
+		<div>
+			<Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={viewBackdrop} onClick={() => dispatch(visibleBackdrop())}>
+				<CircularProgress color="inherit" />
+			</Backdrop>
+		</div>
+	);
+};
