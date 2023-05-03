146f8a22cf3bdc3bea4eb1b61eb468f153b6f7e1
mkrasnansk <miso.krasnansky@gmail.com>
2023-05-03 22:39:58 +0200

    add images

---
+++ src/accessories/images/nftImage/arrImages.js
@@ -0,0 +1,12 @@
+import nftImg1 from "./1.png";
+import nftImg2 from "./2.png";
+import nftImg3 from "./3.png";
+import nftImg4 from "./4.png";
+import nftImg5 from "./5.png";
+import nftImg6 from "./6.png";
+import nftImg7 from "./7.png";
+import nftImg8 from "./8.png";
+export const imageArr = () => {
+	const collectionArr = [nftImg1, nftImg2, nftImg3, nftImg4, nftImg5, nftImg6, nftImg7, nftImg8];
+	return collectionArr;
+};
--- src/components/collection/collectionDetail.js
+++ src/components/collection/collectionDetail.js
@@ -1,12 +1,32 @@
 import { Grid, Typography } from "@mui/material";
-import React from "react";
-
+import React, { useState } from "react";
+import { imageArr } from "../../accessories/images/nftImage/arrImages";
 export const CollectionDetail = () => {
+	const [collection, setCollection] = useState(imageArr);
 	return (
 		<Grid container justifyContent="center">
-				<Grid item>
-					<Typography variant="h1">CollectionDetail</Typography>
-				</Grid>
+			<Grid
+				item
+				sm={12}
+				sx={{
+					display: "flex",
+					justifyContent: "center",
+				}}
+			>
+				<Typography variant="h1">CollectionDetail</Typography>
+			</Grid>
+			{collection.map((item, keyIdx) => {
+				return (
+					<Grid key={keyIdx} justifyContent='center' item xs={12} md={6} xl={3} style={{
+						backgroundImage: `url(${item})`,
+						height:'500px',
+						backgroundRepeat: 'no-repeat',
+						backgroundPosition: 'center',
+						backgroundSize:'contain' //auto|length|cover|contain|intial|inherit
+					}}>
+					</Grid>
+				);
+			})}
 		</Grid>
 	);
 };
